9a8cb24cf4b200e57df164c73aeb154256f9685a
Robson Rodrigues <bobson278@gmail.com>
2018-08-30 00:09:08 -0300

    Projeto iniciado com framework adonis e postgreSQL, criado primeiro controler e rotas e configurado o sqlite

--- start/routes.js
+++ start/routes.js
@@ -1,20 +1,7 @@
-'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.0/routing
-|
-*/
+'use strict'
 
 const Route = use('Route')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
